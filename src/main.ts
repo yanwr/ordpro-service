a873a01fac9849714a8e39689793e52b2a079f4f
Yan Weschenfelder Rodrigues <yanywr@gmail.com>
2023-03-04 21:21:41 -0300

    refactor: coverage and format code

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,50 @@
 import express from "express";
-import pgp from 'pg-promise';
+import pgp from "pg-promise";
 import { validate } from "./CpfValidator";
 
-const connection = pgp()('postgres://postgres:root@localhost:5432/postgres');
+const connection = pgp()("postgres://postgres:root@localhost:5432/ordpro_db");
 
 const app = express();
 app.use(express.json());
 
-const coupons = [
-    { id: 'VALE20', percentage: 20 }
-];
+const coupons = [{ id: "VALE20", percentage: 20 }];
 
+app.get("/health", async function (req, res) {
+  return res.status(200).json({
+    message: "Server is running!",
+  });
+});
 app.post("/checkout", async function (req, res) {
-    const isValidCpf = validate(req.body.cpf);
-    if(!isValidCpf) {
-        res.status(422).json({
-            message: "Invalid cpf"
+  const isValidCpf = validate(req.body.cpf);
+  if (!isValidCpf) {
+    res.status(422).json({
+      message: "Invalid cpf",
+    });
+  } else {
+    let total = 0;
+    for (const item of req.body.items) {
+      const [product] = await connection.query(
+        "SELECT * FROM ccat.product WHERE id = $1",
+        [item.id]
+      );
+      if (product) {
+        total += parseFloat(product?.price) * item.quantity;
+      } else {
+        return res.status(422).json({
+          message: "Product not found",
         });
-    } else {
-        let total = 0;
-        for (const item of req.body.items) {
-            const [ product ] = await connection.query('SELECT * FROM ccat.product WHERE id = $1', [ item.id ]);
-            if (product) {
-                total += parseFloat(product?.price) * item.quantity;
-            } else {
-                return res.status(422).json({
-                    message: 'Product not found'
-                });
-            }
-        }
-        if (req.body.coupon) {
-            const [ coupon ] = await connection.query('SELECT * FROM ccat.coupon WHERE code = $1', [ req.body.coupon ]);
-            if(coupon) {
-                total -= (total * coupon?.percentage) / 100;
-            }
-        }
-        res.json({ total });
+      }
     }
+    if (req.body.coupon) {
+      const [coupon] = await connection.query(
+        "SELECT * FROM ccat.coupon WHERE code = $1",
+        [req.body.coupon]
+      );
+      if (coupon) {
+        total -= (total * coupon?.percentage) / 100;
+      }
+    }
+    res.json({ total });
+  }
 });
 app.listen(3000);
